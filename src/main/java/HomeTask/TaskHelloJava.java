61b45ff16e6d2d6de4a40f16ae6c2d9749a82591
Pavel Davydov <Davydov.works@gmail.com>
2024-01-09 06:28:32 -0500

    add MiddleTask3

---
+++ src/main/java/HomeTask/TaskHelloJava.java
@@ -0,0 +1,9 @@
+package HomeTask;
+
+public class TaskHelloJava {
+    public static void main(String[] args) {
+        String s = "Hello Java";
+        System.out.println("Letter J is the - " + s.indexOf('J') + ", in the lane;");
+        System.out.println("Uppercase string - " + s.toUpperCase());
+    }
+}
