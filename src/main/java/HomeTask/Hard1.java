4383d267cf89daa977cf6faa215a7faeb9c1f954
Pavel Davydov <Davydov.works@gmail.com>
2024-01-09 06:52:21 -0500

    add Hard1

---
+++ src/main/java/HomeTask/Hard1.java
@@ -0,0 +1,8 @@
+package HomeTask;
+
+public class Hard1 {
+    public static void main(String[] args) {
+        String s = "Hello world";
+        System.out.println(" substring from \"Hello world!\" - " + s.substring(6,11));
+    }
+}
